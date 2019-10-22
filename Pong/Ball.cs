ca21ccfe2c9405a8bd93dfb7c80b08bc8a551c73
Michael Gorzelsky <m.d.gorzelsky@gmail.com>
2019-10-21 20:19:31 -0700

    Classes created. Threading set up for input watching. Ball logic started.

---
+++ Pong/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Timers;
+
+namespace Pong
+{
+    class Ball
+    {
+        private Point ballPosition;
+        private int ballDirection;
+        Random rnd = new Random();
+        private readonly Timer timer = new Timer(150);
+
+        public Ball() 
+        {
+            ballDirection = rnd.Next(0,4);
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+        }
+
+
+        public Point BallPosition { get => ballPosition; }
+        
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            switch (ballDirection)
+            {
+                case (0): //up-left
+                    ballPosition.Offset(-1, 1);
+                    break;
+                case (1): //down-left
+                    ballPosition.Offset(-1, -1);
+                    break;
+                case (2): //up-right
+                    ballPosition.Offset(1, 1);
+                    break;
+                case (3): //down-right
+                    ballPosition.Offset(1, -1);
+                    break;
+            }
+        }
+    }
+}
--- Pong/Game.cs
+++ Pong/Game.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Pong
 {
+    public delegate void ThreadStart();
     class Game
     {
+        //Main gameplay loop. Updates every 1/60th of a second for a 60 hz refresh rate.
         public void Start()
+        {
+            Thread inputThread = new Thread(WaitForInput);
+            inputThread.Start();
+            while (true)
+            {
+                
+                Thread.Sleep(1000/60000);
+            }
+        }
+
+        private void WaitForInput()
         {
             while (true)
             {
-
+                Console.WriteLine(Console.ReadKey());
             }
         }
     }
